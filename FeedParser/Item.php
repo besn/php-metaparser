3dbaee375790f46b06dd392fc2993566796dd03b
Andi Mery <besn@besn.at>
2013-12-31 16:14:24 +0100

    renamed $i to $item in the Item class

--- FeedParser/Item.php
+++ FeedParser/Item.php
@@ -16,13 +16,13 @@ class Item extends \FeedParser\Base
   /**
    * @param $feed_type
    */
-  public function __construct($item_type, \SimpleXMLElement $i)
+  public function __construct($item_type, \SimpleXMLElement $item)
   {
     $this->feed_type = $item_type;
     $this->meta_type = 'item';
 
-    if (count($i->children()) > 0) {
-      foreach ($i->children() as $meta_key => $meta_value) {
+    if (count($item->children()) > 0) {
+      foreach ($item->children() as $meta_key => $meta_value) {
         if (isset(\FeedParser\FeedParser::$plugins[$meta_key]) && \FeedParser\FeedParser::$plugins[$meta_key] instanceof \FeedParser\Plugin\Plugin) {
           \FeedParser\FeedParser::$plugins[$meta_key]->processMetaData($this, '', $meta_key, $meta_value);
           \FeedParser\FeedParser::$plugins[$meta_key]->applyMetaData($this);
@@ -34,13 +34,13 @@ class Item extends \FeedParser\Base
     }
 
     // get the namespaces used in the item
-    $namespaces = $i->getNamespaces(true);
+    $namespaces = $item->getNamespaces(true);
 
     // go through the list of used namespaces
     foreach ($namespaces as $ns => $ns_uri) {
       if (isset(\FeedParser\FeedParser::$plugins[$ns]) && \FeedParser\FeedParser::$plugins[$ns] instanceof \FeedParser\Plugin\Plugin) {
-        if (count($i->children($ns, true)) > 0) {
-          foreach ($i->children($ns, true) as $meta_key => $meta_value) {
+        if (count($item->children($ns, true)) > 0) {
+          foreach ($item->children($ns, true) as $meta_key => $meta_value) {
             \FeedParser\FeedParser::$plugins[$ns]->processMetaData($this, $ns, $meta_key, $meta_value);
             unset($meta_key, $meta_value);
           }
